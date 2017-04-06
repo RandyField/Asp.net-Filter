4553a8d1382e0e339ae9897cff3c04042279755a
RandyField <287572291@qq.com>
2017-04-06 14:07:58 +0800

    asp.net filter
    asp.net filter

---
+++ MvcTest/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcTest.Models;
+namespace MvcTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+      
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        [AuthorizeFilter]
+        public ActionResult test()
+        {
+            return Content("有权访问");
+        }
+    }
+}
